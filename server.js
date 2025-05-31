cc3f5751722822105700f4094fc2e37bcc70874d
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2025-05-31 14:35:13 +0100

    a

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 const app = express();
-// set static folder
 app.use(express.static('public'));
-// app.use(express.static(__dirname + '/public'));
-
 app.use(express.urlencoded({
     extended: true
 }));
@@ -13,24 +10,27 @@ const PORT = 3_000;
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT.toLocaleString('en-GB')}`);
 });
-app.get('/users',  async (req, res) =>{
-    // const users = [
-    //     {id: 1, name:'tom'},
-    //     {id: 2, name:'Jack'},
+const fetchUsers = async () => {
 
-    // ];
     const url = 'https://jsonplaceholder.typicode.com/users';
     const response = await fetch(url);
     const users = await response.json();
-    res.send(/*html */`
-        <h1 class="text-2xl font-bold my-4">
-            Users
-        </h1>
+    return users;
+
+}
+app.get('/users',  async (req, res) =>{
+     fetchUsers().then(users => {
+
+        res.send(/*html */`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
         <ul>
             ${users.map(user => `<li> ${user.name} </li>`).join('')}
             
         </ul>
         
       `);
+
+     });
+    
 
 });
